02539c4a9b42f644b5e9bc8ec5a30ea6a9a31c7b
Anton Sotnik <toxanetoxa@gmail.com>
2024-05-11 17:10:21 +0300

    show departamentsId

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
-require('dotenv').config();
-
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
+
+// Проверка наличия переменных окружения
+if (!process.env.INPUT_JSON_FILE || !process.env.OUTPUT_JSON_FILE) {
+    console.error("Необходимо указать пути к входному и выходному файлам в переменных окружения.");
+    process.exit(1);
+}
+
+// Чтение входных данных из файла
 const rawData = fs.readFileSync(process.env.INPUT_JSON_FILE);
 const data = JSON.parse(rawData);
-const outputDir = './output';
 
-const users = []
-data["employees"].forEach(el => {
-    let workPlace = []
+// Функция для извлечения списка идентификаторов должностей из профиля и мест работы
+function extractJobTitleIds(employee) {
+    const jobTitleIds = new Set();
 
-    if (el["profile"]) {
-         el.profile.forEach(item => {
-             workPlace.push(item["jobTitleId"])
-        })
+    if (employee.profile) {
+        employee.profile.forEach(item => {
+            jobTitleIds.add(item.jobTitleId);
+        });
     }
 
-    if (el["workPlaces"]) {
-        el["workPlaces"].forEach(item => {
-            workPlace.push(item["jobTitleId"])
-        })
+    if (employee.workPlaces) {
+        employee.workPlaces.forEach(item => {
+            jobTitleIds.add(item.jobTitleId);
+        });
     }
 
-    users.push({
-        "fio": el["fio"],
-        "userName": el["userName"],
-        "jobTitleIds": Array.from(new Set(workPlace))
+    return Array.from(jobTitleIds);
+}
+
+const extractDepartmentId = (employee) => {
+    const departmentIds = new Set
+    if (!employee["workPlaces"].length) return null
+
+    employee["workPlaces"].forEach((el) => {
+        departmentIds.add(el["departmentId"])
     })
-})
 
-const selectedData = {
-    users: users
-};
+    return Array.from(departmentIds)
+}
 
-// Проверить существование директории output
+// Формирование списка пользователей с их данными
+const users = data.employees.map(employee => ({
+    fio: employee.fio,
+    userName: employee.userName,
+    jobTitleIds: extractJobTitleIds(employee),
+    departmentIds: extractDepartmentId(employee)
+}));
+
+// Формирование объекта с выбранными данными
+const selectedData = { users };
+
+// Создание директории output, если она не существует
+const outputDir = './output';
 if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir);
 }
 
+// Запись выбранных данных в выходной файл
 const outputFile = path.join(outputDir, process.env.OUTPUT_JSON_FILE);
 fs.writeFileSync(outputFile, JSON.stringify(selectedData, null, 4));
 console.log('Файл успешно записан в директорию output.');
